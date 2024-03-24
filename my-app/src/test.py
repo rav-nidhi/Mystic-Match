dbb1a56e220c45163268d7188f731cb74404f860
Nidhi <nidhiravala@gmail.com>
2024-03-23 22:38:02 -0500

    update test file

--- my-app/src/test.py
+++ my-app/src/test.py
@@ -1,3 +1,4 @@
-datapath = r'C:\Users\Nidhi\Documents\WEHack\my-app\src\data\orgs.csv'
-
 import pandas
+datapath = r'C:\Users\Nidhi\Documents\WEHack\my-app\src\data\orgs.csv'
+data = pandas.read_csv(datapath)
+print(data)
